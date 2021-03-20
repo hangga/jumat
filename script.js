f25ed20780d11c5c8610526d6a29f333d51d114b
Hangga Aji Sayekti <bazeniancode@gmail.com>
2021-03-21 06:06:13 +0700

    add hijri

--- script.js
+++ script.js
@@ -76,16 +76,21 @@ function generate() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
     
     //Initialize header
     cell1.innerHTML = "<h3>No.<h3/>";
     cell1.style['width'] = '20px';
-    cell2.innerHTML = "<h3>Tanggal<h3/>";
-    cell2.style['width'] = '200px';
-    cell3.innerHTML = "<h3>Pasaran<h3/>";
-    cell3.style['width'] = '150px';
-    cell4.innerHTML = "<h3>Imam dan Khotib<h3/>";
-    cell4.style['width'] = '300px';
+    cell2.innerHTML = "<h3>Masehi<h3/>";
+    cell2.style['width'] = '150px';
+
+    cell3.innerHTML = "<h3>Hijriah<h3/>";
+    cell3.style['width'] = '170px';
+
+    cell4.innerHTML = "<h3>Pasaran<h3/>";
+    cell4.style['width'] = '80px';
+    cell5.innerHTML = "<h3>Imam dan Khotib<h3/>";
+    cell5.style['width'] = '320px';
 
     var currentValue = parseInt(selyear.options[selyear.selectedIndex].value);
     var d2 = new Date("2014/01/27");
@@ -112,14 +117,16 @@ function generate() {
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
             var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
 
             cell1.innerHTML = nomor;
             cell2.innerHTML = x.getDate() + " " + (bulan[x.getMonth()]) + " " + (x.getFullYear());
-            cell3.innerHTML = pasaran[selisih % 5];
+            cell3.innerHTML = writeIslamicDate(x, -1);
+            cell4.innerHTML = pasaran[selisih % 5];
 
             var divN = document.createElement("div");
             divN.setAttribute("id", "div" + nomor);
-            cell4.appendChild(divN);
+            cell5.appendChild(divN);
 
             var editN = document.createElement("input");
             editN.setAttribute("id", "editN" + nomor);
@@ -137,4 +144,102 @@ function generate() {
             x = new Date(x.getTime() + (24 * 60 * 60 * 1000));
         }
     }
+}
+
+// Credit to https://www.al-habib.info/islamic-calendar/hijricalendartext.htm
+function gmod(n,m){
+    return ((n%m)+m)%m;
+}
+
+function kuwaiticalendar(date, adjust){
+    
+    var today = date;
+    if(adjust) {
+        adjustmili = 1000*60*60*24*adjust; 
+        todaymili = today.getTime()+adjustmili;
+        today = new Date(todaymili);
+    }
+    day = today.getDate();
+    month = today.getMonth();
+    year = today.getFullYear();
+    m = month+1;
+    y = year;
+    if(m<3) {
+        y -= 1;
+        m += 12;
+    }
+
+    a = Math.floor(y/100.);
+    b = 2-a+Math.floor(a/4.);
+    if(y<1583) b = 0;
+    if(y==1582) {
+        if(m>10)  b = -10;
+        if(m==10) {
+            b = 0;
+            if(day>4) b = -10;
+        }
+    }
+
+    jd = Math.floor(365.25*(y+4716))+Math.floor(30.6001*(m+1))+day+b-1524;
+
+    b = 0;
+    if(jd>2299160){
+        a = Math.floor((jd-1867216.25)/36524.25);
+        b = 1+a-Math.floor(a/4.);
+    }
+    bb = jd+b+1524;
+    cc = Math.floor((bb-122.1)/365.25);
+    dd = Math.floor(365.25*cc);
+    ee = Math.floor((bb-dd)/30.6001);
+    day =(bb-dd)-Math.floor(30.6001*ee);
+    month = ee-1;
+    if(ee>13) {
+        cc += 1;
+        month = ee-13;
+    }
+    year = cc-4716;
+
+    if(adjust) {
+        wd = gmod(jd+1-adjust,7)+1;
+    } else {
+        wd = gmod(jd+1,7)+1;
+    }
+
+    iyear = 10631./30.;
+    epochastro = 1948084;
+    epochcivil = 1948085;
+
+    shift1 = 8.01/60.;
+    
+    z = jd-epochastro;
+    cyc = Math.floor(z/10631.);
+    z = z-10631*cyc;
+    j = Math.floor((z-shift1)/iyear);
+    iy = 30*cyc+j;
+    z = z-Math.floor(j*iyear+shift1);
+    im = Math.floor((z+28.5001)/29.5);
+    if(im==13) im = 12;
+    id = z-Math.floor(29.5001*im-29);
+
+    var myRes = new Array(8);
+
+    myRes[0] = day; //calculated day (CE)
+    myRes[1] = month-1; //calculated month (CE)
+    myRes[2] = year; //calculated year (CE)
+    myRes[3] = jd-1; //julian day number
+    myRes[4] = wd-1; //weekday number
+    myRes[5] = id; //islamic date
+    myRes[6] = im-1; //islamic month
+    myRes[7] = iy; //islamic year
+
+    return myRes;
+}
+function writeIslamicDate(date, adjustment) {
+    //var wdNames = new Array("Ahad","Ithnin","Thulatha","Arbaa","Khams","Jumuah","Sabt");
+    var iMonthNames = new Array("Muharram","Shafar","Rabi'ul Awwal","Rabi'ul Akhir",
+    "Jumadal Ula","Jumadal Akhir","Rajab","Sya'ban",
+    "Ramadhan","Syawwal","Dhulqa'adah","Dhulhijah");
+    var iDate = kuwaiticalendar(date, adjustment);
+    var outputIslamicDate = /*wdNames[iDate[4]] + ", " +*/ iDate[5] + " " + iMonthNames[iDate[6]] + " " + iDate[7];
+    return outputIslamicDate;
 }
